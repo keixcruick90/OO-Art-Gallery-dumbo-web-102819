db1e528f57d8aa2b0a79aa238a8c518efb5537f9
Keillan Cruickshank <cruickshankxkeillan90@gmail.com>
2019-12-04 21:38:59 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,43 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select { |painting| painting.artist == self}
+  end
+
+  def galleries
+    paintings.map { |painting| painting.gallery}
+  end
+
+  def cities
+    galleries.map { |gallery| gallery.city}
+  end
+
+  def self.total_experience
+    @all_total_experience = []
+    @all_total_experience << artist.years_experience.sum
+    @all_total_experience.sum
+  end
+
+  def self.most_prolific
+    self.max_by { |artist| self.total_experience}
+  end
+
+  def create_painting(title, price, gallery)
+      Painting.new(title, price, self, gallery)
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,22 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("Juno", 19)
+a2 = Artist.new("Vela", 36)
 
+g1 = Gallery.new("Mizar's Palace", "Space")
+g2 = Gallery.new("Goldwood", "Etheria")
+g3 = Gallery.new("SS Anubis", "Ocean")
 
+p1 = a1.create_painting("Mizar", 10000, a1, g1)
+p1 = a1.create_painting("Ant Eater", 500, a1, g1)
+p1 = a1.create_painting("Tribal", 900000, a1, g2)
+p1 = a1.create_painting("Lupus", 628000880, a1, g3)
+p1 = a2.create_painting("Tri-Rocket Launcher", 10891596968, a2, g1)
+p1 = a2.create_painting("Sniper Rifle", 20004000500, a2, g1)
+p1 = a2.create_painting("Magnus", 7776689646858, a2, g2)
+p1 = a2.create_painting("Thorax", 628196472, a2, g2)
+p1 = a2.create_painting("Battle Cruiser", 15680204626803, a2, g3)
+p1 = a2.create_painting("Invincibility", 99999999999999999999, a2, g3)
 
 binding.pry
 
